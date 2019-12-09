8155cb66c30a6087d90f766516d0dd4704a95a43
Pierre-Alain <ouvrard.pierre.alain@gmail.com>
2019-12-09 13:01:10 +0900

    release v0.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, aergo.io'
 author = 'aergo.io'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
