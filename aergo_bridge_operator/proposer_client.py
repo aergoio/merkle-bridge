d3cc8e6bd6d219f8ed8c318686dc96199a1c32b5
Pierre-Alain <ouvrard.pierre.alain@gmail.com>
2019-12-13 11:22:57 +0900

    change logs path

--- aergo_bridge_operator/proposer_client.py
+++ aergo_bridge_operator/proposer_client.py
@@ -54,8 +54,9 @@ file_formatter = logging.Formatter(
 stream_formatter = logging.Formatter('%(threadName)s: %(message)s')
 
 
-root_dir = os.path.dirname(__file__)
-file_handler = logging.FileHandler(root_dir + '/logs/proposer.log')
+log_file_path = 'logs/proposer.log'
+os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
+file_handler = logging.FileHandler(log_file_path)
 file_handler.setFormatter(file_formatter)
 
 stream_handler = logging.StreamHandler()
